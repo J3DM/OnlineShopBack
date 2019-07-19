7f6480a16ebf337bd7a3f0d5bedaae1f64e043af
J3DM <josemdmarcos@gmail.com>
2019-07-19 19:58:35 +0200

    Base service working with ping test route

---
+++ init.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+const cors = require("cors")
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use("/v1", require("./entities/routes"))
+
+const mongoUrl="mongodb://localhost:27017/OnlineShop"
+const nodePort=3000
+
+app.listen(nodePort, () => {
+    mongoose.connect(
+        mongoUrl,
+        {useNewUrlParser: true, useCreateIndex: true}
+    )
+    .then(
+        (connection)=>{
+            if(!connection) throw "Error connecting to the database"
+            console.log("Conectado a la base de datos")
+            console.log(`Escuchando puerto`,nodePort)
+        }
+    )
+    .catch(
+        (error)=>{
+            console.log(error)
+        }
+    )
+    
+  })
